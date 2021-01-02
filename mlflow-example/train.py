562a0fcefc833c37814d2afbe9f824017c05e584
Andrey Grabovoy <grabovoy@ap-team.ru>
2021-01-03 01:24:29 +0300

    fixed

--- mlflow-example/train.py
+++ mlflow-example/train.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 
 os.environ["AWS_ACCESS_KEY_ID"] = 'MY_KEY'
 os.environ["AWS_SECRET_ACCESS_KEY"] = 'MY_SECRET_KEY'
-os.environ["AWS_DEFAULT_REGION"] = 'MY_REGION'
+os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
 
 
 if __name__ == "__main__":
