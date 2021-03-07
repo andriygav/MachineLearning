e98c52f61d1893a4244763529701e52d98c7c311
andriygav <andriy.graboviy@mail.ru>
2021-03-07 18:26:19 +0300

    fix

--- docker-processing/src/processing/processing/service.py
+++ docker-processing/src/processing/processing/service.py
@@ -9,7 +9,7 @@ from grpc_reflection.v1alpha import reflection
 
 from configobj import ConfigObj
 from prometheus_client import start_http_server, Summary, Counter
-from py_grpc_prometheus.prometheus_server_interceptor import PromServerInterceptor
+from python_grpc_prometheus.prometheus_server_interceptor import PromServerInterceptor
 
 from processing_protos.processing_pb2 import Request, Response, DESCRIPTOR
 from processing_protos.processing_pb2_grpc import (ProcessorServiceStub, 
@@ -75,8 +75,7 @@ def serve(config):
         options=[('grpc.max_send_message_length', MAX_MSG_LENGTH),
                  ('grpc.max_message_length', MAX_MSG_LENGTH),
                  ('grpc.max_receive_message_length', MAX_MSG_LENGTH)],
-        interceptors=(PromServerInterceptor(
-        	enable_handling_time_histogram=True, legacy=True),) 
+        interceptors=(PromServerInterceptor(),) 
     )
     add_ProcessorServiceServicer_to_server(ProcessorService(), server)
     SERVICE_NAMES = (
